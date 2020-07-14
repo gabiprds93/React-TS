1c577100777a0ffb339082bc26dc43e93952abea
Gabriela <gabiprds.93@gmail.com>
2020-07-13 23:25:46 -0500

    add Component MoviesList

---
+++ src/components/organisms/MoviesList.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+import MovieModel from '../../models/Movie'
+import Movie from '../molecules/Movie'
+
+interface IMoviesListProps {
+  movies: MovieModel[];
+}
+
+const MoviesList: React.FC<IMoviesListProps> = ({movies}) => {
+  return(
+    <ul>
+      {movies.map((item, index) => {
+        return(
+          <li key={index}>
+            <Movie movie={item}></Movie>
+          </li>
+        )
+      })}
+    </ul>
+  )
+}
+
+export default MoviesList;
